efb1beae85b5688b1b8340baf51d8a2ed33081aa
Sergey Bochkov <bochkovsa@gmail.com>
2019-01-03 13:04:11 +0500

    responsive Advantage block

--- src/components/AdvRow.vue
+++ src/components/AdvRow.vue
@@ -1,19 +1,30 @@
 <template>
-  <el-row :gutter="20" v-if="index % 2 == 0">
-    <el-col class="hidden-md-and-up" :md="8" :lg="8" :xl="8">
-      <img :src="img">
-    </el-col>
-    <el-col :md="16" :lg="16" :xl="16" v-html="text"/>
-    <el-col class="hidden-sm-and-down" :md="8" :lg="8" :xl="8">
-      <img :src="img">
-    </el-col>
-  </el-row>
-  <el-row :gutter="20" v-else>
-    <el-col :md="8" :lg="8" :xl="8">
-      <img :src="img">
-    </el-col>
-    <el-col :md="16" :lg="16" :xl="16" v-html="text"/>
-  </el-row>
+  <div>
+    <div class="hidden-md-and-up">
+      <el-row>
+        <el-col>
+          <img :src="img">
+        </el-col>
+      </el-row>
+      <el-row>
+        <el-col class="pad" v-html="text"/>
+      </el-row>
+    </div>
+    <div class="hidden-sm-and-down">
+      <el-row v-if="index % 2 == 0">
+        <el-col :md="16" :lg="16" :xl="16" v-html="text"/>
+        <el-col :md="8" :lg="8" :xl="8">
+          <img :src="img">
+        </el-col>
+      </el-row>
+      <el-row v-else>
+        <el-col :md="8" :lg="8" :xl="8">
+          <img :src="img">
+        </el-col>
+        <el-col :md="15" :lg="15" :xl="15" :offset="1" v-html="text"/>
+      </el-row>
+    </div>
+  </div>
 </template>
 
 <script>
@@ -28,10 +39,16 @@ img {
     margin-left: auto;
     margin-right: auto;
 }
-.el-row {
-    padding: 10px 10px;
+.hidden-md-and-up .el-row {
+    padding: 0;
 }
-.el-col {
+.hidden-md-and-up .pad {
+    padding: 20px;
+}
+.hidden-sm-and-down .el-row {
+    padding: 20px;
+}
+.hidden-sm-and-down .el-col {
     padding: 10px 20px;
     vertical-align: top;
 }
